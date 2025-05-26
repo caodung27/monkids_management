880f6313e43750f89fbfa159bce2d853eef499bb
caodung27 <cdnh1234567890@gmail.com>
2025-05-26 14:14:40 +0000

    update config db

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,14 +1,10 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'monkid_management',
+  host: process.env.DATABASE_HOST || '172.31.14.122',
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'postgres',
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.ts,.js}'],
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
-  synchronize: false,
-}; 
+  synchronize: process.env.NODE_ENV !== 'production',
+};
