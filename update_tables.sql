08f702b7059adc18ad141ad38850134383031162
caodung227 <cdnh1234567890@gmail.com>
2025-06-04 06:03:07 +0700

    public repo v2

--- update_tables.sql
+++
@@ -1,4 +0,0 @@
--- Thêm cột mới vào bảng app_teacher
-ALTER TABLE public.app_teacher
-ADD COLUMN probation_days INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN probation_salary NUMERIC(10,2) NOT NULL DEFAULT 0;
